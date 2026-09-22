839acad8c7e1ce7bf69ae8a3ad4cd03e4344092e
Prasad Hule <prasad.hule001@stud.fh-dortmund.de>
2026-09-22 19:48:39 +0530

    task 4

--- Week 1/Task4/task4.py
+++ Week 1/Task4/task4.py
@@ -2,8 +2,8 @@
 
 n = int(input("Input a number to calculate the factorial of: "))
 
-fakultaet = 1
+factorial = 1
 for i in range(1, n + 1):
-    fakultaet *= i
+    factorial *= i
 
-print("The factorial of " + str(n) + " is: " + str(fakultaet))
+print("The factorial of " + str(n) + " is: " + str(factorial))
